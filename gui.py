aa7a578176398e753f86adcbf142fcdb7f8b0fec
slocksert <rsdlj01@gmail.com>
2024-05-22 19:19:51 -0300

    removed comments

--- gui.py
+++ gui.py
@@ -12,34 +12,27 @@ def select_directory(entry):
 def start_program(entry):
     home_directory = entry.get()
     if not home_directory:
-        home_directory = os.path.expanduser("~")  # Obtém a pasta home do usuário
+        home_directory = os.path.expanduser("~") 
     run_main(home_directory if home_directory else None)
 
 def create_gui():
-    # Criar janela principal
     window = tk.Tk()
     window.title("File Organizer")
 
-    # Criar rótulo
     label = tk.Label(window, text="Select your home directory")
     label.pack()
 
-    # Criar entrada de texto
     entry = tk.Entry(window, width=50)
     entry.pack()
 
-    # Definir a pasta home do usuário como valor padrão para a entrada
     entry.insert(0, os.path.expanduser("~"))
 
-    # Criar botão para selecionar diretório
     select_button = tk.Button(window, text="Select Directory", command=lambda: select_directory(entry))
     select_button.pack()
 
-    # Criar botão para iniciar o programa
     start_button = tk.Button(window, text="Start Program", command=lambda: start_program(entry))
     start_button.pack()
 
-    # Rodar aplicação
     window.mainloop()
 
 if __name__ == "__main__":
